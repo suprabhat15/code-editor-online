97a3b120a8518e2d47971362ec039966b0f7e723
suprabhat25 <suprabhat2018@gmail.com>
2021-12-08 01:45:54 +0530

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,101 @@
+import React, { useMemo, useState } from "react";
+import { Flex } from "@chakra-ui/layout";
+import axios from "axios";
+import crypto from "crypto";
+
+const Home = () => {
+  const [language, setLanguage] = useState<
+    "c" | "c++" | "javascript" | "python"
+  >("c");
+
+  const [code, setCode] = useState<string>("");
+  const [output, setOutput] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+
+  const runCode = async () => {
+    const formData = {
+      language,
+      code,
+    };
+
+    setLoading(true);
+
+    let requestId = localStorage.getItem("requestId");
+
+    if (!requestId) {
+      requestId = crypto.randomBytes(6).toString("hex");
+
+      localStorage.setItem("requestId", requestId);
+    }
+
+    await axios.post(
+      process.env.REACT_APP_API_URL + `/api/generateCode/${requestId}`,
+      formData
+    );
+
+    getOutput();
+  };
+
+  const getOutput = async () => {
+    setLoading(true);
+    let requestId = localStorage.getItem("requestId");
+
+    if (!requestId) {
+      requestId = crypto.randomBytes(6).toString("hex");
+
+      localStorage.setItem("requestId", requestId);
+    }
+
+    setTimeout(async () => {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + `/api/output/${requestId}`
+      );
+
+      setOutput(response.data);
+      setLoading(false);
+    }, 1000);
+  }
+
+  const clearOutput = () => {
+    setOutput("");
+  };
+
+  useMemo(() => {
+    if (language === "c") {
+      setCode(`#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello world");
+
+    return 0;
+}`);
+    }
+
+    if (language === "javascript") {
+      setCode(`console.log("Welcome Developers");`);
+    }
+
+    if (language === "python") {
+      setCode(`print("Hello world")`);
+    }
+
+    if (language === "c++") {
+      setCode(`#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+
+    return 0;
+}`);
+    }
+  }, [language]);
+
+  return (
+    <Flex w="full" h="100vh">
+    </Flex>
+  );
+};
+
+export default Home;
